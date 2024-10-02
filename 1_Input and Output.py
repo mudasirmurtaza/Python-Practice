98303664e92da428862edfd99a6328bb7fa999f9
Mudasir Murtaza <109586296+mudasirmurtaza@users.noreply.github.com>
2024-10-02 14:38:19 +0500

    Create 1_Input and Output.py

---
+++ 1_Input and Output.py
@@ -0,0 +1,21 @@
+"""
+INPUT IN PYTHON
+Input( ) statement is used to accept values (using the keyboard) from the user 
+input( )  #result for input( ) is always a str
+int ( input( ) )  #int
+float ( input( ) )   #float
+
+OUTPUT IN PYTHON
+Print() Inside the parenthesis you can write whatever you wanna print
+"""
+
+# String input
+name = input("name:")
+
+#int input
+age = int (input("age :"))
+
+#float input
+price = float(input("price: "))
+  
+print("My name is", name, "and I am", age, "years old")
