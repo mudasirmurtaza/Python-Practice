91c12e1c009890ecf1e042ac52ecdd32ee56d758
Mudasir Murtaza <109586296+mudasirmurtaza@users.noreply.github.com>
2024-10-04 11:05:45 +0500

    Create 2_Varialbe_output.py

---
+++ 2_Varialbe_output.py
@@ -0,0 +1,11 @@
+name = "Mudasir" # String data type, used for storing characters.
+age = 21         #Int data type, used to store integer values
+price = 20.99    # Float data tupe, used to store decimal values
+
+print (age)   
+print (name)
+print (price)
+
+
+print ("My name is :", name)
+print ("My age is:", age)
